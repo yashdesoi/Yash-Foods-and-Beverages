a908951adfceb34498f2beb1a9660f12a8deb362
YasH <yashdesoi@gmail.com>
2021-03-27 21:35:03 +0530

    misc updates

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,6 +2,28 @@ const bcrypt = require('bcrypt');
 
 const Customer = require('../models/Customer');
 
+const errorHandler = function(err) {
+    const feedback = {
+        name: '',
+        email: '',
+        'mobile-number': '',
+        address: '',
+        password: ''
+    };
+
+    if (err.code === 11000) {
+        feedback.email = 'That email is already registered'
+    }
+
+    for (let key in err.errors) {
+        const {properties} = err.errors[key];
+        const {path, message} = properties;
+        feedback[path] = message;
+    }
+
+    console.log(feedback);
+};
+
 const signup_get = (req, res) => {
     res.render('signup');
 };
@@ -12,7 +34,7 @@ const signup_post = (req, res) => {
             res.status(201).json(doc);
         })
         .catch(err => {
-            console.log(err);
+            errorHandler(err);
             res.status(400).json({success: false});
         });
 };
@@ -21,26 +43,25 @@ const login_get = (req, res) => {
     res.render('login');
 };
 
-const login_post = (req, res) => {
+const login_post = async (req, res) => {
     const {password, email} = req.body;
-    Customer.findOne({email})
-        .then(doc => {
-            if (doc) {
-                return bcrypt.compare(password, doc.password);
+
+    try {
+        const doc = await Customer.findOne({email});
+
+        if (doc) {
+            const isEqual = await bcrypt.compare(password, doc.password);
+            if (isEqual) {
+                res.json(doc);
             } else {
-                res.status(400).json({success: false});
+                throw Error('Incorrect password');
             }
-        })
-        .then(result => {
-            if (result) {
-                res.json({success: true});
-            } else {
-                res.status(400).json({success: false});
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
+        } else {
+            throw Error('Incorrect email');
+        }
+    } catch (err) {
+        res.status(400).json({success: false});
+    }
 };
 
 module.exports = {
